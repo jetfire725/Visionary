a4e818f57311ce47783cdbf3a10182c06b0ce123
David Moore <dmoore42n81@gmail.com>
2019-03-16 20:29:05 -0400

    Reworked camera intent to get original image instead of thumbnail. Added sharpen function in ocr class to attempt to sharpen image before feeding to tesseract. Function did not improve accuracy. Although the camera rework got us a sharper image, my OCR confidence decreased by 20%. Don't know why.

--- app/src/main/java/com/example/visionary/MainActivity.java
+++ app/src/main/java/com/example/visionary/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.visionary;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -16,6 +18,8 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
+    Uri imageUri;
+
     //Currently, this activity's only purpose is the capture the image and start the next activity.
 
     @Override
@@ -23,17 +27,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent x = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Code for original Image:
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+        imageUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+
+//        Intent x = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         //File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
-        startActivityForResult(x, 1);
+        startActivityForResult(intent, 1);
     }
 
     @Override
     protected  void onActivityResult(int requestCode, int resultCode, Intent x){
         if (requestCode == 1 && resultCode == RESULT_OK){
-            Bundle extras = x.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            magicBox(imageBitmap);
+//            Bundle extras = x.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            Bitmap imageBitmap = null;
+            try {
+                imageBitmap = MediaStore.Images.Media.getBitmap(
+                        getContentResolver(), imageUri);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if(imageBitmap != null) {
+                magicBox(imageBitmap);
+            } else {
+                Log.v("My Tag", "ERROR: ImageBitmap is null");
+            }
+
 
 
 
