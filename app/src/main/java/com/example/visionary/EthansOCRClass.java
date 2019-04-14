d37bef847840ba9a24fb12a24a42b38cfdbfde8d
Ethan Bateman <jetfire725@hotmail.com>
2019-04-13 23:08:14 -0400

    Fixed new OCR class to create training file.

--- app/src/main/java/com/example/visionary/EthansOCRClass.java
+++ app/src/main/java/com/example/visionary/EthansOCRClass.java
@@ -7,33 +7,39 @@ import android.os.Environment;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class EthansOCRClass {
     Bitmap srcBitmap;
-    String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/TesseractSample/";
-    String TESSDATA = "tessdata";
+    Context context;
 
-
-    public EthansOCRClass(Bitmap b, Context context) {
+    public EthansOCRClass(Bitmap b, Context c) {
         this.srcBitmap = b;
-
-
+        this.context=c;
     }
 
-    public String performOCR() {
-        TessBaseAPI tessBaseApi;
-
+    public String performOCR() throws IOException {
+        File f = new File(Environment.getExternalStorageDirectory().toString() + "/TesseractSample/tessdata");
+        if (!f.exists()){
+            f.mkdirs();
+            //read in stream
+            InputStream in = context.getAssets().open("tessdata/eng.traineddata");
+            byte[] buf = new byte[in.available()];
+            in.read(buf);
+            in.close();
+            //write out stream
+            OutputStream out = new FileOutputStream(Environment.getExternalStorageDirectory().toString() + "/TesseractSample/tessdata/eng.traineddata");
+            out.write(buf);
+            out.close();
+        }
         System.out.println("Perform OCR Called");
-
-        File dir = new File(DATA_PATH + TESSDATA);
-
-        tessBaseApi = new TessBaseAPI();
-        tessBaseApi.init(DATA_PATH, "eng");
+        TessBaseAPI tessBaseApi = new TessBaseAPI();
+        tessBaseApi.init(Environment.getExternalStorageDirectory().toString() + "/TesseractSample/", "eng");
         tessBaseApi.setImage(this.srcBitmap);
         return tessBaseApi.getUTF8Text();
-
-
-
     }
 
 }
